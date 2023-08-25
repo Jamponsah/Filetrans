ed87e4061ef940caf22ff34b6db2f4389b5fe889
Jamponsah <john.amponsah@azubiafrica.org>
2023-08-25 12:10:53 +0000

    Jamponsah

---
+++ mercury.py
@@ -0,0 +1,15 @@
+# Step 1
+print("Welcome to mercury group of azubi africa")
+print(" We as mercury group have learnt so much about fast tracking our careers in the world  of tech")
+
+# Step 2: Story 1
+Story1 = input("story1:store your story variable")
+print('story1')
+
+# Step 3: Inputs
+noun = input("give me a noun:  ")
+verb = input(" give me a verb: ")
+adjectives = input("give mea adjectives: ")
+
+# Step 4:  Story Generation
+print(f"{noun} {verb} {adjectives}")
